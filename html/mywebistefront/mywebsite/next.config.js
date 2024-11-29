611638507afac5d04eb6148886e159b70c4ea3a0
wnlhdx <314460007@qq.com>
2024-11-29 20:07:19 +0800

    问题调试

--- html/mywebistefront/mywebsite/next.config.js
+++ html/mywebistefront/mywebsite/next.config.js
@@ -3,7 +3,6 @@
 const nextConfig = {}
 
 module.exports = {
-  swcMinify: false,  // 禁用 swc 因为Android上的Swc目前还有问题
   webpack(config, { isServer }) {
     // 在服务器端使用 Babel，客户端使用默认配置
     if (!isServer) {
