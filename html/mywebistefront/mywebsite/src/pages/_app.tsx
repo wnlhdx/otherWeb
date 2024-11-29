4822d6ec7ed387fa3eb4224a7857862346466e03
wnlhdx <314460007@qq.com>
2024-11-29 20:40:39 +0800

    修改两个项目

--- html/mywebistefront/mywebsite/src/pages/_app.tsx
+++ html/mywebistefront/mywebsite/src/pages/_app.tsx
@@ -1,6 +1,7 @@
 import { ThemeProvider, createTheme } from '@mui/material/styles'
 import CssBaseline from '@mui/material/CssBaseline'
 import { AppProps } from 'next/app'
+import React from 'react';
 
 const theme = createTheme({
 
