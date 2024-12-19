44ffdc62cc4ee8f855aa250117768a6e918fe06a
wnlhdx <314460007@qq.com>
2024-12-20 00:51:17 +0800

    extension

--- html/mywebistefront/extension/aitest/src/extension.ts
+++ html/mywebistefront/extension/aitest/src/extension.ts
@@ -1,26 +1,74 @@
-// The module 'vscode' contains the VS Code extensibility API
-// Import the module and reference it with the alias vscode in your code below
 import * as vscode from 'vscode';
-
+import axios from 'axios';
 // This method is called when your extension is activated
 // Your extension is activated the very first time the command is executed
+
+
 export function activate(context: vscode.ExtensionContext) {
+    console.log('Congratulations, your extension "aitest" is now active!');
 
-	// Use the console to output diagnostic information (console.log) and errors (console.error)
-	// This line of code will only be executed once when your extension is activated
-	console.log('Congratulations, your extension "aitest" is now active!');
+    // 注册代码补全提供者
+    const provider = vscode.languages.registerCompletionItemProvider('javascript', {
+        provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+            // 在输入的光标位置提供补全建议
+            const completionItems: vscode.CompletionItem[] = [];
 
-	// The command has been defined in the package.json file
-	// Now provide the implementation of the command with registerCommand
-	// The commandId parameter must match the command field in package.json
-	const disposable = vscode.commands.registerCommand('aitest.helloWorld', () => {
-		// The code you place here will be executed every time your command is executed
-		// Display a message box to the user
-		vscode.window.showInformationMessage('Hello World from aitest!');
-	});
+            // 添加一些示例代码补全项
+            const item1 = new vscode.CompletionItem('console.log');
+            item1.detail = 'Log to console';
+            item1.documentation = 'Outputs a message to the console';
+            item1.insertText = 'console.log($1)';
+            item1.kind = vscode.CompletionItemKind.Function;
 
-	context.subscriptions.push(disposable);
+            completionItems.push(item1);
+
+            const item2 = new vscode.CompletionItem('alert');
+            item2.detail = 'Show alert message';
+            item2.documentation = 'Displays an alert in the browser';
+            item2.insertText = 'alert($1)';
+            item2.kind = vscode.CompletionItemKind.Function;
+
+            completionItems.push(item2);
+
+            return completionItems;
+        }
+    });
+
+    // 订阅代码补全提供者
+    context.subscriptions.push(provider);
 }
+
+
+async function getAICodeCompletion(query: string): Promise<string> {
+    // 向 AI API 请求代码补全
+    const response = await axios.post('https://api.gptgod.online', {
+        model: 'gpt-4-all',  // 你可以选择 OpenAI 的合适模型
+        prompt: query,
+        max_tokens: 50
+    }, {
+        headers: {
+            'Authorization': `sk-OsMMq65tXdfOIlTUYtocSL7NCsmA7CerN77OkEv29dODg1EA`
+        }
+    });
+
+    return response.data.choices[0].text.trim();
+}
+
+const provider = vscode.languages.registerCompletionItemProvider('javascript', {
+    async provideCompletionItems(document: vscode.TextDocument, position: vscode.Position) {
+        const completionItems: vscode.CompletionItem[] = [];
+
+        const query = document.getText(document.getWordRangeAtPosition(position));
+        const aiCompletion = await getAICodeCompletion(query);
+
+        const item = new vscode.CompletionItem(aiCompletion);
+        item.kind = vscode.CompletionItemKind.Text;
+        completionItems.push(item);
+
+        return completionItems;
+    }
+});
+
 
 // This method is called when your extension is deactivated
 export function deactivate() {}
